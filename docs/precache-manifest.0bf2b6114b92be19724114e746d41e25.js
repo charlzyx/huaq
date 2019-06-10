f0e8bda8aad8517e46e4514b3e3991627f199b7b
xiaochao.yang <xiaochao.yang@bianlifeng.com>
2019-06-10 21:13:50 +0800

    fix pages

---
+++ docs/precache-manifest.0bf2b6114b92be19724114e746d41e25.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fba95a94237b59635a21",
+    "url": "css/app.0ac64dc5.css"
+  },
+  {
+    "revision": "fba95a94237b59635a21",
+    "url": "js/app.bff9dc6c.js"
+  },
+  {
+    "revision": "1000d166ab07824914cf",
+    "url": "css/chunk-vendors.eae9fff2.css"
+  },
+  {
+    "revision": "1000d166ab07824914cf",
+    "url": "js/chunk-vendors.10b7b99a.js"
+  },
+  {
+    "revision": "a0bdd804ca30f2e313d82870152accb4",
+    "url": "index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  }
+];
--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,8 @@
 const path = require('path');
 
+const HOST = './';
+
 module.exports = {
   outputDir: path.resolve(__dirname, 'docs'),
+  publicPath: `${HOST}`,
 };
